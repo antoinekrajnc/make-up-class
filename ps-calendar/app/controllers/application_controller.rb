4fbbca4264abf3174c9a43f0fa3c85af2c89c2b7
antoinekrajnc <antoinekrajnc@gmail.com>
2017-03-24 20:14:45 -0700

    created a dropdown menu to select cohort

--- ps-calendar/app/controllers/application_controller.rb
+++ ps-calendar/app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   class ApplicationController < ActionController::API
     include CanCan::ControllerAdditions
   end
@@ -10,4 +11,14 @@ class ApplicationController < ActionController::Base
     flash.keep(:notice)
     redirect_to root_url
   end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_cohort])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:user_cohort])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:user_name])
+  end
+
 end
--- ps-calendar/config/initializers/devise.rb
+++ ps-calendar/config/initializers/devise.rb
@@ -277,19 +277,6 @@ Devise.setup do |config|
 
   extend ActiveSupport::Concern
 
-    included do
-      before_action :configure_permitted_parameters
-    end
-
-    protected
-
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:user_cohort])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:user_cohort])
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:user_name])
-    end
-
   end
 
 DeviseController.send :include, DevisePermittedParameters
