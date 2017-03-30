89484c85eed3526f7e2149a60f218b86631141d5
antoinekrajnc <antoinekrajnc@gmail.com>
2017-03-29 18:48:28 -0700

    postgres conversion

--- Gemfile
+++ Gemfile
@@ -41,8 +41,6 @@ gem 'jbuilder', '~> 2.5'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
-  # Use sqlite3 as the database for Active Record
-  gem 'sqlite3'
 end
 
 group :production do
@@ -60,3 +58,5 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+ruby "2.3.1"
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170316222521) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cohorts", force: :cascade do |t|
     t.string   "title"
     t.integer  "batch_number"
@@ -78,8 +81,8 @@ ActiveRecord::Schema.define(version: 20170316222521) do
     t.datetime "updated_at",                          null: false
     t.string   "user_cohort"
     t.string   "user_name"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
